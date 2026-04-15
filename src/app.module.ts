7ae1d8f2bfd757d67556d258cec9b74ec72a2312
talya cohen <talyaacc055@gmail.com>
2026-04-15 13:36:39 +0300

    change the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
 import { DocumentModule } from './documents/documents.module';
 
 @Module({
-  imports: [PrismaModule,DocumentModule],
+  imports: [DocumentModule],
   controllers: [AppController],
   providers: [AppService],
 })
